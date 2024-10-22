d4d4346765b0eb1b4900f4a67c6c828e1528bd93
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-22 01:17:34 -0500

    Implement the Messages and Conversations 02
    Start implementing the message routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth-routes.js";
+import messageRoutes from "./routes/message-routes.js";
+// import conversationRoutes from "./routes/conversation-routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,7 +13,11 @@ dotenv.config();
 
 // using middleware
 app.use(express.json()); // to parse incoming request with JSON payloads (from req.body)
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+// app.use("/api/conversation", conversationRoutes);
 
 // app.get("/", (req, res) => {
 //   // root route http://localhost:5000/
