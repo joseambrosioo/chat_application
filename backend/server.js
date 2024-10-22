d6898c3670fff258ac906cb73dd7f62142c88f03
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-21 20:55:49 -0500

    Set up the Project 02
    Continue setting up the project

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,32 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth-routes.js";
 
 const app = express();
 
-app.listen(5000, () => console.log("Server Running on port 5000"));
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  // root route http://localhost:5000/
+  res.send("Hello World");
+});
+
+// without using middleware
+// app.get("/api/auth/sigup", (req, res) => {
+//   console.log("signup route");
+// });
+
+// app.get("/api/auth/login", (req, res) => {
+//   console.log("login route");
+// });
+// ``;
+// app.get("/api/auth/logout", (req, res) => {
+//   console.log("logout route");
+// });
+
+// using middleware
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
