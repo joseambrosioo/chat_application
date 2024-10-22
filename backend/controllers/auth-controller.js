fe6a36bcada646777224d37f7559e066e1482352
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-21 23:22:02 -0500

    Implement the User Authentication 04
    Start implementing the auth controller's sign-in function

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -7,6 +7,11 @@ import generateTokenAndSetCookie from "../utils/generateToken.js";
 //   console.log("signupUser");
 // };
 
+// export const login = (req, res) => {
+//   res.send("loginUser");
+//   console.log("loginUser");
+// };
+
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
@@ -60,9 +65,32 @@ export const signup = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("loginUser");
-  console.log("loginUser");
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcryptjs.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password!" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      userName: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const logout = (req, res) => {
