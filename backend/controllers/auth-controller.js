6063fcb1a78e511cee16d69ad5ba2194a96763db
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-21 22:35:21 -0500

    Implement the User Authentication 02
    Start implementing the auth controller's signup function

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -3,10 +3,49 @@
 //   console.log("signupUser");
 // };
 
+import User from "../models/user-model.js";
+
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
-  } catch (error) {}
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords do not match!" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "User already exists!" });
+    }
+
+    // Hash passsword here
+    //https://avatar-placeholder.iran.liara.run/
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      userName: newUser.username,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const login = (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,18 @@ import authRoutes from "./routes/auth-routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  // root route http://localhost:5000/
-  res.send("Hello World");
-});
+dotenv.config();
 
 // using middleware
+app.use(express.json()); // to parse incoming request with JSON payloads (from req.body)
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   // root route http://localhost:5000/
+//   res.send("Hello World");
+// });
 
 // app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
 
