47abc72db8f3492b706098bd93f44cfb8c1ac5c0
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-22 04:34:09 -0500

    Implement the UI 01
    Start implementing the UI

---
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -0,0 +1,39 @@
+import Messages from "./Messages";
+
+const MessageContainer = () => {
+  return (
+    <div className="md:min-w-[450px] flex flex-col">
+      <>
+        {/* Header */}
+        <div className="bg-slate-500 px-4 py-2 mb-2">
+          <span className="label-text">To:</span>
+          <span className="text-gray-900 font-bold">Jose Ambrosio</span>
+        </div>
+
+        <Messages />
+      </>
+    </div>
+  );
+};
+export default MessageContainer;
+
+// STARTER CODE SNIPPET
+// import MessageInput from "./MessageInput";
+// import Messages from "./Messages";
+
+// const MessageContainer = () => {
+// 	return (
+// 		<div className='md:min-w-[450px] flex flex-col'>
+// 			<>
+// 				{/* Header */}
+// 				<div className='bg-slate-500 px-4 py-2 mb-2'>
+// 					<span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>Jose Ambrosio</span>
+// 				</div>
+
+// 				<Messages />
+// 				<MessageInput />
+// 			</>
+// 		</div>
+// 	);
+// };
+// export default MessageContainer;
