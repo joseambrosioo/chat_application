7706f212562e386155d3283d0b260fbc3914385f
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-21 23:07:08 -0500

    Implement the User Authentication 03
    Implement the auth controller's signup function's password hashing and jwt

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -1,9 +1,11 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user-model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
+
 // export const signup = (req, res) => {
 //   res.send("signupUser");
 //   console.log("signupUser");
 // };
-
-import User from "../models/user-model.js";
 
 export const signup = async (req, res) => {
   try {
@@ -20,6 +22,10 @@ export const signup = async (req, res) => {
     }
 
     // Hash passsword here
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    // User profiles default picture
     //https://avatar-placeholder.iran.liara.run/
 
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
@@ -28,19 +34,25 @@ export const signup = async (req, res) => {
     const newUser = new User({
       fullName,
       username,
-      password,
+      password: hashedPassword,
       gender,
       profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
     });
 
-    await newUser.save();
+    if (newUser) {
+      // Generate JWT token here
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
 
-    res.status(201).json({
-      _id: newUser._id,
-      fullName: newUser.fullName,
-      userName: newUser.username,
-      profilePic: newUser.profilePic,
-    });
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        userName: newUser.username,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid User Data" });
+    }
   } catch (error) {
     console.log("Error in signup controller", error.message);
 
