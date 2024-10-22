c80597ab2758c9dda8ff6d9106b305919fdb79da
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-21 23:27:56 -0500

    Implement the User Authentication 05
    Start implementing the auth controller's sign-out function

--- backend/controllers/auth-controller.js
+++ backend/controllers/auth-controller.js
@@ -12,6 +12,11 @@ import generateTokenAndSetCookie from "../utils/generateToken.js";
 //   console.log("loginUser");
 // };
 
+// export const logout = (req, res) => {
+//   res.send("logoutUser");
+//   console.log("logoutUser");
+// };
+
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
@@ -94,6 +99,12 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.send("logoutUser");
-  console.log("logoutUser");
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully." });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
