4c1df9159a7f08cf640a1bebf7bb0ec87d29dcf8
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-23 03:39:05 -0500

    Implement the socket.io 02
    Continue implementing the socket.io

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -13,20 +13,29 @@ const io = new Server(server, {
 // 	return userSocketMap[receiverId];
 // };
 
+// export const getReceiverSocketId = (receiverId) => {
+//   return userSocketMap[receiverId];
+// };
+
+export const getReceiverSocketId = (receiverId) => {
+  return userSocketMap[receiverId] || null;
+};
+
 const userSocketMap = {}; // userId : socket Id
 
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId != "undefined") userSocketMap[userId] = socket.id;
+  //   if (userId != "undefined") userSocketMap[userId] = socket.id;
+  if (userId) userSocketMap[userId] = socket.id;
 
   // io.emit() is user to listen to th events to all connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   // socket.on() is used to listen to the events. can be used both on cliendt and server side
   socket.on("disconnect", () => {
-    console.log("user disconnected", socket.io);
+    console.log("user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
