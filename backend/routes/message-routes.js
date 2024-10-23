dee602729181998db99dd4a302d5d2a3e44b90c1
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-10-23 00:18:30 -0500

    Implement the get-messages functionality 01
    Start implementing the get-messages functionality

--- backend/routes/message-routes.js
+++ backend/routes/message-routes.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
+// router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
