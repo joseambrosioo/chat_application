dd7b3ac22564f57b07202c499293f9859c9e7864
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-12-23 23:51:21 -0800

    Update Home.jsx
    Updated Home.jsx

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,9 +4,13 @@ import Sidebar from "../../components/sidebar/Sidebar.jsx";
 const Home = () => {
   return (
     // <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
-    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden">
-      <Sidebar />
-      <MessageContainer />
+    // <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden">
+    //   <Sidebar />
+    //   <MessageContainer />
+    // </div>
+    <div className="flex flex-col sm:flex-row sm:h-auto md:h-[550px] rounded-lg overflow-hidden">
+      <Sidebar className="w-full sm:w-[200px] md:w-[250px]" />
+      <MessageContainer className="w-full sm:flex-1 md:w-[calc(100%-250px)]" />
     </div>
   );
 };
