34416876f44fb94edcfe64a150334a682f795330
Jose Nazareno Torres Ambrosio <j_ambrosio@mail.fhsu.edu>
2024-12-26 02:01:34 -0800

    Update Sidebar.jsx
    Updated Sidebar.jsx

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -5,7 +5,8 @@ import SearchInput from "./SearchInput";
 const Sidebar = () => {
   return (
     // <div className="border-r border-slate-500 p-4 flex flex-col text-black">
-    <div className="border-r-0 sm:border-r border-slate-500 p-4 flex flex-col text-black">
+    // <div className="border-r-0 sm:border-r border-slate-500 p-4 flex flex-col text-black">
+    <div className="border-r-0 sm:border-r border-b sm:border-b-0 border-slate-500 p-4 flex flex-col text-black">
       <SearchInput />
       <div className="divider px-3"></div>
       <Conversations />
